fc03dcae94e567fb08091900b7c5a87797c8bafe
Choco1125 <sebascruz981125@gmail.com>
2023-02-05 16:46:07 -0500

    feat: :sparkles: Creating searchbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import { Link } from 'wouter'
+import React, { useState } from 'react'
+import { Link, useLocation } from 'wouter'
 
 const POPULARES = ["Matrix", "Chile", "Colombia", "Ecuador"]
 
 export default function Home() {
+
+    const [keyword, setKeyword] = useState('')
+    const [path, pushLocation] = useLocation()
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        pushLocation(`/search/${keyword}`)
+    }
+
+    const handleChange = event => {
+        setKeyword(event.target.value)
+    }
+
     return (
         <>
+            <form onSubmit={handleSubmit}>
+                <input type="search" placeholder='Search a gif here...' value={keyword} onChange={handleChange} />
+            </form>
             <h3>Los gifs más populares</h3>
             <ul>
                 {

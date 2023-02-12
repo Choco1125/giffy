e98deb907a173a024cd5d90860b3322a0c2a83d0
Choco1125 <sebascruz981125@gmail.com>
2023-02-12 01:19:08 -0500

    feat: :sparkles: Carga peresoza de tendencias

--- src/components/TrendingSearches/index.jsx
+++ src/components/TrendingSearches/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Category from '../Category'
 import getTrendingTerms from 'services/getTrendingTerms'
 
-export default function TrendingSearches() {
+function TrendingSearches() {
     const [trends, setTrends] = useState([])
 
 
@@ -11,4 +11,27 @@ export default function TrendingSearches() {
     }, [])
 
     return <Category categories={trends} />
+}
+
+export default function LazyTrending() {
+    const [show, setShow] = useState(false)
+
+    useEffect(() => {
+        const onChange = (entries) => {
+            const element = entries[0]
+            if (element.isIntersecting) {
+                setShow(true)
+            }
+        }
+
+        const observer = new IntersectionObserver(onChange, {
+            rootMargin: '100px'
+        })
+
+        observer.observe(document.getElementById('lazyTrending'))
+    }, [])
+
+    return <div id="lazyTrending">
+        {show ? <TrendingSearches /> : null}
+    </div >
 }

dd7f8e30a70a1bd52ac6ee5b66aac62a56ebe817
Choco1125 <sebascruz981125@gmail.com>
2023-03-05 02:04:24 -0500

    fix: :bug: Fixing loading single gif on reload page

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import useGlobalGifs from 'hooks/useGlobalGifs'
 import Gif from 'components/Gif'
+import useSingleGif from 'hooks/useSingleGif'
+import Spinner from 'components/Spinner'
+import { Redirect } from 'wouter'
 
 export default function Detail({ params }) {
 
-    const gifs = useGlobalGifs()
-    const gif = gifs.find(singleGif => singleGif.id == params.id)
+    const { gif, isLoading, isError } = useSingleGif({ id: params.id })
+
+    if (isLoading) return <Spinner />
+    if (isError) return <Redirect to='/404' />
+    if (!gif) return null
 
     return <Gif id={gif.id} title={gif.title} url={gif.url} />
 }

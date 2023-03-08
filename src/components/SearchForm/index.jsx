1829e55bb839faa7c66dec4a168c8520e6934c81
Choco1125 <sebascruz981125@gmail.com>
2023-03-07 22:49:13 -0500

    feat: :art: Moving reducer to custom hook

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,69 +1,46 @@
-import React, { useReducer } from 'react'
+import React from 'react'
 import { useLocation } from 'wouter'
+import useForm from './hooks/useForm'
 
 const RATINGS = ['g', 'pg', 'pg-13', 'r']
 
-const ACTIONS = {
-    UPDATE_KEYWORD: 'update_keyword',
-    UPDATE_RATING: 'update_rating',
-}
-
 function SearchForm({ initialKeyword = '', initialRating = 'g' }) {
+  const [path, pushLocation] = useLocation()
 
-    const reducer = (state, action) => {
+  const { keyword, rating, updateKeyword, updateRating } = useForm({
+    initialKeyword,
+    initialRating,
+  })
 
-        switch (action.type) {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    pushLocation(`/search/${keyword}/${rating}`)
+  }
 
-            case ACTIONS.UPDATE_KEYWORD:
-                return {
-                    ...state,
-                    keyword: action.payload,
-                }
+  const handleChange = (event) => {
+    updateKeyword(event.target.value)
+  }
 
-            case ACTIONS.UPDATE_RATING:
-                return {
-                    ...state,
-                    rating: action.payload,
-                }
+  const handleChangeRating = (event) => {
+    updateRating(event.target.value)
+  }
 
-            default:
-                return state
-        }
-
-    }
-
-    const [path, pushLocation] = useLocation()
-
-    const [state, dispatch] = useReducer(reducer, {
-        keyword: decodeURIComponent(initialKeyword),
-        rating: initialRating,
-    })
-
-    const { keyword, rating } = state
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        pushLocation(`/search/${keyword}/${rating}`)
-    }
-
-    const handleChange = (event) => {
-        dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: event.target.value })
-    }
-
-    const handleChangeRating = (event) => {
-        dispatch({ type: ACTIONS.UPDATE_RATING, payload: event.target.value })
-
-    }
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type="search" placeholder='Search a gif here...' value={keyword} onChange={handleChange} />
-            <select value={rating} onChange={handleChangeRating}>
-                <option disabled>Rating type</option>
-                {RATINGS.map(rating => <option key={rating}>{rating}</option>)}
-            </select>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Search a gif here..."
+        value={keyword}
+        onChange={handleChange}
+      />
+      <select value={rating} onChange={handleChangeRating}>
+        <option disabled>Rating type</option>
+        {RATINGS.map((rating) => (
+          <option key={rating}>{rating}</option>
+        ))}
+      </select>
+    </form>
+  )
 }
 
 export default React.memo(SearchForm)
